ebec8443a6c70641f1e4eb5dfe6eefc41b45d022
Esteban Rodriguez <nahuelr.developer@gmail.com>
2023-09-06 15:55:09 -0300

    create edit user

--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { Users } = require("../models");
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Users.update(req.body, { where: { id }, returning: true }).then((result) => {
+    const [rowCounter, user] = result;
+
+    if (!user[0]) {
+      return res
+        .status(404)
+        .json({ message: "The user was not found with that id" });
+    }
+
+    res.status(200).json(user[0]);
+  });
+});
 
 module.exports = router;
