414cd00667a93dd102993885241d4d18930e8a27
b1ractive <kevin.jn99@gmail.com>
2023-09-06 15:46:30 -0300

    logout created from auth.routes

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const token = require("../config/token");
+const cookieParser = require("cookie-parser");
+const validateCookie = require("../middlewares/auth.middlewares");
+
+router.use(cookieParser());
+
+router.post("/logout", (req, res) => {
+  res.clearCookie("token").sendStatus(204);
+});
 
 module.exports = router;
