49b1735c939e57a0a01aefc6ab822c03e5c1dddf
Tute22 <agustinsandoval1601@gmail.com>
2023-09-06 15:12:07 -0300

    create routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-//rutas
+const authRouter = require("./auth.routes");
+const productsRouter = require("./products.routes");
+const usersRouter = require("./users.routes");
+
+router.use("/auth", authRouter);
+router.use("/products", productsRouter);
+router.use("/users", usersRouter);
 
 module.exports = router;
