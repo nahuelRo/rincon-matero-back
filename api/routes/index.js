a6fc74c5e4619a052cb11370799ae16eda1d1919
b1ractive <kevin.jn99@gmail.com>
2023-09-11 17:29:14 -0300

    deletion of the category as admin

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,13 +5,12 @@ const authRouter = require("./auth.routes");
 const productsRouter = require("./products.routes");
 const usersRouter = require("./users.routes");
 const categoriesRouter = require("./categories.routes");
-
 const checkoutRouter = require("./checkout.routes");
+
 router.use("/auth", authRouter);
 router.use("/products", productsRouter);
 router.use("/users", usersRouter);
 router.use("/categories", categoriesRouter);
-
 router.use("/checkout", checkoutRouter);
 
 module.exports = router;
