e3a9b174e4ced8e962220b538b90426ee25cdba8
Braian Barrientos <braianbts@gmail.com>
2023-09-12 16:51:45 -0300

    route categories modification finished

--- api/routes/categories.routes.js
+++ api/routes/categories.routes.js
@@ -56,4 +56,23 @@ router.delete("/:categoryId", isAdmin, (req, res) => {
   });
 });
 
+///RUTA MODIFICAR CATEGORIA
+router.put("/:categoryId", isAdmin, async (req, res) => {
+  const { categoryId } = req.params;
+  const { name, description } = req.body;
+
+  try {
+    const category = await Categories.findByPk(categoryId);
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    await category.update({ name, description });
+    res.status(200).json({ message: "Category name and description updated" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 module.exports = router;
